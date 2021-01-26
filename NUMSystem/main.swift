3a2961ec4219d21b63004795ac38e783db20662d
Benjamn <bfreeman@lcs.on.ca>
2021-01-26 14:53:03 -0500

    Initial Commit

---
+++ NUMSystem/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  NUMSystem
+//
+//  Created by Ben Freeman on 2021-01-26.
+//
+
+import Foundation
+
+/// INPUT
+// get the "from" number system
+print("What number system do you want to convert from")
+print("B: Binary")
+print("O: Octal")
+print("D: Decimal")
+print("H: Hexadecmial")
+print("Enter your choice (B/O/D/H): ", terminator: "") // Keep following output
+                                                       // on the same line
+let from = readLine()!
+/// PROCESS
+print(from)
+
+
+
+/// OUTPUT
